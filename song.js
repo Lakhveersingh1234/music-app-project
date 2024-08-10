33602f867c550f4df0e29684deee9834c83736a1
Lakhbir <LakhbirSingh1234@gmail.com>
2024-08-10 20:23:48 +0530

    h

--- song.js
+++ song.js
@@ -2,6 +2,10 @@ let songname=document.querySelector("#songname");
 let singername=document.querySelector("#singername");
 let songimage=document.querySelector(".img");
 let playpause=document.querySelector("#play-pause");
+let volumecontrol=document.querySelector('#vol-range');
+let songRange=document.querySelector('.vol-range');
+let volumemute=document.querySelector('#volumesvg');
+
 let index=0;
 let playingsong=false;
 let track=document.createElement("audio")
@@ -30,6 +34,14 @@ function loadtrack(index){
    songname.innerHTML=songs[index].name;
    singername.innerHTML=songs[index].singername;
    songimage.style=`background-image: url("${songs[index].image}");`
+   volume()
+   duration()
+   setInterval(()=>{
+    songRange.max=track.duration
+    songRange.value=track.currentTime
+   },1000)
+   track.load()
+
 }
 loadtrack(index);
 
@@ -46,7 +58,7 @@ function playPause(){
 function playSong(){
     track.play();
     playingsong=true;
- playpause.src="pause.png";
+    playpause.src="pause.png";
 
 }
 
@@ -80,4 +92,16 @@ function previous(){
         playSong()
 
     }
+}
+function volume(){
+    track.volume=volumecontrol.value/100
+    if(volumecontrol.value==0){
+         volumemute.src="mute.png"
+    }
+    else{
+        volumemute.src="volume.png"
+    }
+}
+function duration(){
+    track.currentTime=songRange.value;
 }
